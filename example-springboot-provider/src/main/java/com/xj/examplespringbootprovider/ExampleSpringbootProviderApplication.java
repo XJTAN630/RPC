606039a1d1571911f575148e28c5a4fc90fef944
XiaoJia <xiaojia_tip@163.com>
2025-02-23 18:58:56 +0800

    配置加载实现,维护全局配置对象

---
+++ example-springboot-provider/src/main/java/com/xj/examplespringbootprovider/ExampleSpringbootProviderApplication.java
@@ -0,0 +1,20 @@
+package com.xj.examplespringbootprovider;
+
+import com.xj.rpc.springboot.starter.annotation.EnableRpc;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 示例 Spring Boot 服务提供者应用
+ *
+
+ */
+@SpringBootApplication
+@EnableRpc
+public class ExampleSpringbootProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExampleSpringbootProviderApplication.class, args);
+    }
+
+}
---
+++ example-springboot-provider/src/main/java/com/xj/examplespringbootprovider/UserServiceImpl.java
@@ -0,0 +1,21 @@
+package com.xj.examplespringbootprovider;
+
+import com.xj.example.common.model.User;
+import com.xj.example.common.service.UserService;
+import com.xj.rpc.springboot.starter.annotation.RpcService;
+import org.springframework.stereotype.Service;
+
+/**
+ * 用户服务实现类
+ *
+
+ */
+@Service
+@RpcService
+public class UserServiceImpl implements UserService {
+
+    public User getUser(User user) {
+        System.out.println("用户名：" + user.getName());
+        return user;
+    }
+}
